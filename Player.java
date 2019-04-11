6807f519f3495a3f222f37acb10a39f100c30e76
Richard Wang <40673516+Frostcyanide@users.noreply.github.com>
2019-04-11 08:33:28 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Player {
-
+	
+	private int balance;
+	
+	private List<Tile> tiles;
+	
+	private String name;
+	
+	public Player(String n) {
+		balance=1500;
+		name = n;
+		tiles=new ArrayList<Tile>();
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getBalance() {
+		return this.balance;
+	}
+	
+	private int roll() {
+		Random gen = new Random();
+		return (gen.nextInt(6)+1)*2;
+		
+	}
+	
+	
+	
+	
+	
+	
 }
