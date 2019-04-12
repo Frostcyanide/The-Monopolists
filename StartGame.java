d67bb8eb7a4303b6d5947b92735c7d948dd2e9c7
sameervp100 <49522166+sameervp100@users.noreply.github.com>
2019-04-12 09:00:18 -0400

    StartGame.java

--- StartGame.java
+++ StartGame.java
@@ -1,9 +1,29 @@
-
+import java.util.Scanner;
 public class StartGame {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String []args)
+	{
+		 
+		int numberOfPlayers = 0;
+		System.out.println("How many players (2-4): ");
+		Scanner input = new Scanner(System.in);
+		numberOfPlayers = input.nextInt();
+		if(numberOfPlayers == 2)
+		{
+			Player player1 = new Player();
+			Player player2 = new Player();
+		}
+		else if(numberOfPlayers == 3)
+		{
+			Player player1 = new Player();
+			Player player2 = new Player();
+			Player player3 = new Player();
+		}
+		else
+		{
+			Player player1 = new Player();
+			Player player2 = new Player();
+			Player player3 = new Player();
+			Player player4 = new Player();
+		}
 	}
-
 }
