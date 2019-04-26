5424ab709447a91102981d8314a84fafe30e0a63
Richard Wang <40673516+Frostcyanide@users.noreply.github.com>
2019-04-26 08:44:07 -0400

    Update Chance.java

--- Chance.java
+++ Chance.java
@@ -1,4 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Chance {
+	private List<String> deck = new ArrayList<String>();
 
+	public Chance() {
+		deck.add("Advance to Payday, collect $200\n" + "");
+		deck.add("Advance to the nearest unowned property, you can buy it if you wish\n" + "");
+		deck.add("Move backwards 5 spots\n" + "");
+		deck.add(
+				"Move to the nearest property that an opponent owns, if no opponents own any properties, then you do not move\n"
+						+ "");
+		deck.add("Advance to the Castle, if it is unowned, you may purchase it.\n" + "");
+	}
 }
