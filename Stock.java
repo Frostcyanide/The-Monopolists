1f833d21336af1dd107b2ef2f6175cc435303f60
sameervp100 <49522166+sameervp100@users.noreply.github.com>
2019-04-26 08:46:56 -0400

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -1,4 +1,10 @@
 
 public class Stock {
-
+	public void stocks()
+	{
+		Stock Amazon = new Stock();
+		Stock GeneralMotors = new Stock();
+		Stock McDonalds = new Stock();
+		Stock Microsoft = new Stock();
+	}
 }
