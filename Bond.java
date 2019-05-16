36a1cc5a79241ec82ff54c09469699cec623e0b4
Richard Wang <40673516+Frostcyanide@users.noreply.github.com>
2019-05-16 08:18:20 -0400

    Add files via upload

--- Bond.java
+++ Bond.java
@@ -2,18 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bond extends Tile {
-	
-	
-	
-	
+
+	private boolean paid = false;
+	private int remainT;
 
 	public Bond(String name, int price, String color, int index) {
 		super(name, price, color, index);
+		remainT = 3;
+
 	}
-	
-	
-	
-	
-	
+
+	public int payday() {
+
+		if (remainT == 0) {
+			if (paid == false) {
+				paid = true;
+				remainT = -1;
+				return price * 2;
+			}
+		}
+		return 0;
+
+	}
+
+	public void remainT() {
+		remainT--;
+	}
+
+	@Override
+	public int compareTo(Tile o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
